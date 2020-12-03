385f40e49c78aee5d8b4b6064872ec267ad45e34
Technote-limited <66956083+Technote-limited@users.noreply.github.com>
2020-12-03 15:06:37 +0300

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,8 +4,8 @@ define('EMAIL','njerimargret70@gmail.com');
 define('PASSWORD','GithukaCfthn/7048');
 
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','members_project');
+define('DB_HOST','us-cdbr-east-02.cleardb.com');
+define('DB_USER','bce3bbaf4df4c4');
+define('DB_PASS','5569113d');
+define('DB_NAME','heroku_1568ec2897f403e');
 
